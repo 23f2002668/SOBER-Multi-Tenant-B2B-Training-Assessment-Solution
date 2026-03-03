884b0993cd5792b08ff3da030a00e0ea4dfea8e0
Shahzada Moon <shahzadamoon@Shahzadas-MacBook-Air.local>
2026-03-03 20:52:46 +0530

    Initial commit

--- app.py
+++ app.py
@@ -6,17 +6,7 @@ from flask_login import login_required, LoginManager
 from flask_mail import Mail, Message
 from flask_sqlalchemy import SQLAlchemy
 import os, subprocess, random, time, sqlite3, json, numpy as np, matplotlib, matplotlib.pyplot as plt; matplotlib.use('Agg')
-from string import ascii_letters
 from datetime import datetime, date
-from werkzeug.security import generate_password_hash, check_password_hash
-from threading import Thread
-from reportlab.lib import pdfencrypt, colors
-from reportlab.lib.units import inch
-from reportlab.lib.pagesizes import A4
-from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Image, Spacer, Table, TableStyle
-from reportlab.pdfbase import pdfmetrics
-from reportlab.lib.enums import TA_JUSTIFY, TA_CENTER, TA_RIGHT, TA_LEFT
 
 # Load environment variables
 # load_dotenv()
